d8a08b00f9026d0e64130dda145543148b8397b0
metacious <miraunmax@hotmail.com>
2023-11-09 00:28:41 -0400

    Modelos agregados, función self pendiente

--- preentrega3/models.py
+++ preentrega3/models.py
@@ -2,11 +2,31 @@ from django.db import models
 
 # Create your models here.
 # Cada modelo representa una base de datos.
-def Heroe():
-    pass
+class Heroe(models.Model):
+    nombre = models.CharField(max_length=32)
+    tipo = models.CharField(max_length=16)
+    aporte = models.CharField(max_length=16)
+    vida = models.IntegerField(default=500) 
+    velocidad = models.IntegerField(default=300)
 
-def Arma():
-    pass
+    def __str__(self):
+        return self
 
-def Consumible():
-    pass
+
+class Arma(models.Model):
+    nombre = models.CharField(max_length=32)
+    rareza = models.CharField(max_length=16)
+    descripcion = models.CharField(max_length=256)
+    costo = models.IntegerField(default=1000)
+
+    def __str__(self):
+        return self
+
+class Consumible(models.Model):
+    nombre = models.CharField(max_length=32)
+    rareza = models.CharField(max_length=16)
+    descripcion = models.CharField(max_length=256)
+    costo = models.IntegerField(default=1000)
+
+    def __str__(self):
+        return self
