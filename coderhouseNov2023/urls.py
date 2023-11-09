0d22435d7beee59d05ed8b9e8d0db9b9d2f529a4
metacious <miraunmax@hotmail.com>
2023-11-09 01:06:44 -0400

    Se agregan vistas y html para crear y buscar los datos. Además se va armando los bloques de HTML heredados de layout.html para cada archivo html, empezando con las páginas de listas

--- coderhouseNov2023/urls.py
+++ coderhouseNov2023/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from preentrega3.views import dota, lista_heroes, lista_armas, lista_consumibles
+from preentrega3.views import dota, lista_heroes, lista_armas, lista_consumibles, crear_heroe, crear_arma, crear_consumible, buscar_heroe, buscar_arma, buscar_consumible
 
 
 urlpatterns = [
@@ -26,4 +26,10 @@ urlpatterns = [
     path('preentrega3/', lista_heroes, name="heroes"),
     path('preentrega3/', lista_armas, name="armas"),
     path('preentrega3/', lista_consumibles, name="consumibles"),
+    path('preentrega3/', crear_heroe, name="crear-heroe"),
+    path('preentrega3/', crear_arma, name="crear-arma"),
+    path('preentrega3/', crear_consumible, name="crear-consumible"),
+    path('preentrega3/', buscar_heroe, name="buscar-heroe"),
+    path('preentrega3/', buscar_arma, name="buscar-arma"),
+    path('preentrega3/', buscar_consumible, name="buscar-consumible"),
 ]
--- preentrega3/views.py
+++ preentrega3/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import loader
+from django.urls import reverse
 
 from .models import Heroe, Arma, Consumible
 
@@ -98,4 +99,71 @@ def lista_consumibles(request):
     )
     # Devuelve la variable http_response
     return http_response
+
+# Vistas para crear héroes, armas o consumibles.
+def crear_heroe(request):
+    if request.method == "POST":
+        data = request.POST
+        heroe = Heroe(
+            #nombre = data[nombre],
+            #tipo = data[tipo],
+            #aporte = data[aporte],
+            #vida = data[vida],
+            #velocidad = data[velocidad]
+            )
+        heroe.save()
+        exito = reverse("lista-heroes")
+    else:
+        return render(
+            request=request,
+            template_name="crear-heroe.html"
+        )
+
+def crear_arma(request):
+    if request.method == "POST":
+        data = request.POST
+        arma = Arma(
+            #nombre = data[nombre],
+            #tipo = data[tipo],
+            #aporte = data[aporte],
+            #vida = data[vida],
+            #velocidad = data[velocidad]
+            )
+        arma.save()
+        exito = reverse("lista-armas")
+    else:
+        return render(
+            request=request,
+            template_name="crear-arma.html"
+        )
+
+def crear_consumible(request):
+    if request.method == "POST":
+        data = request.POST
+        consumible = Consumible(
+            #nombre = data[nombre],
+            #tipo = data[tipo],
+            #aporte = data[aporte],
+            #vida = data[vida],
+            #velocidad = data[velocidad]
+            )
+        consumible.save()
+        exito = reverse("lista-consumibles")
+    else:
+        return render(
+            request=request,
+            template_name="crear-consumible.html"
+        )
+
+# Vistas para buscar héroes, armas o consumibles.
+def buscar_heroe(request):
+    pass
+
+def buscar_arma(request):
+    pass
+
+def buscar_consumible(request):
+    pass
+
+
 
