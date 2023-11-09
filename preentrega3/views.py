96913d919f0e71c39de563cd6a2ea676d186d7af
metacious <miraunmax@hotmail.com>
2023-11-08 22:35:50 -0400

    Se agregan vistas de pagina de inicio, paginas de formularios y paginas con las listas

--- preentrega3/views.py
+++ preentrega3/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+# Página de inicio
+def dota(request):
+    return HttpResponse("Aquí está la lista de campeones de DotA")
+
+# Vistas de formularios
+def formulario_heroes(request):
+    return HttpResponse("Aquí se coloca template")
+
+def formulario_armas(request):
+    return HttpResponse("Las armas del juego")
+
+def formulario_consumibles(request):
+    return HttpResponse("Los objetos que se consumen en el juego")
+
+# Vistas de listas
+def lista_heroes(request):
+    return HttpResponse("Aquí se coloca template")
+
+def lista_armas(request):
+    return HttpResponse("Las armas del juego")
+
+def lista_consumibles(request):
+    return HttpResponse("Los objetos que se consumen en el juego")
+
